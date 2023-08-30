568f2ba2dfd338911d2d57919723ac010d30628a
Alec <106216445+FreshMozzarella@users.noreply.github.com>
2023-08-30 15:37:11 -0400

    change fetch endpoint for email

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -17,7 +17,7 @@ function EmailForm() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('/send-email', {
+            const response = await fetch('/.netlify/functions/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
